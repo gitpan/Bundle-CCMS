23748c209ad51b6032b7075ef96ef1f9681cc64a
Joshua I. Miller <unrtstRMSPAM@gmail.com>
2005-02-01 16:31:10 +0000

    Import of UNRTST/Bundle-CCMS-1.01 from CPAN.
    gitpan-cpan-distribution: Bundle-CCMS
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         UNRTST/Bundle-CCMS-1.01.tar.gz
    gitpan-cpan-author:       UNRTST
    gitpan-cpan-maturity:     released

---
+++ CCMS.pm
@@ -0,0 +1,71 @@
+package Bundle::CCMS;
+use strict;
+use warnings;
+
+$VERSION = '1.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::CCMS - A bundle to install modules required for CCMS (Call Center Management System).
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::CCMS'
+
+=head1 CONTENTS
+
+CGI
+
+Cwd
+
+Compress::Zlib
+
+Config::Tiny
+
+Data::Serializer
+
+Data::Dumper
+
+DBI
+
+File::Spec
+
+File::Basename
+
+FreezeThaw
+
+Getopt::Long
+
+Mail::Internet
+
+MIME::Base64
+
+MIME::Lite
+
+Sys::Syslog
+
+Text::CSV
+
+Text::Wrap
+
+Time::CTime
+
+Time::ParseDate
+
+Time::Local
+
+=head1 DESCRIPTION
+
+This is a bundle to modules required for CCMS (Call Center Management System).
+
+It's likely of little use to anyone unless they're installing CCMS.
+
+=head1 AUTHOR
+
+Joshua I. Miller, E<lt>ccmsbundle at calltech dot comE<gt>
+
+=cut 
